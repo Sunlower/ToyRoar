ca9f4c3eaba12fc67de2951513598e3fd0108435
Ieda Xavier <92699834+Sunlower@users.noreply.github.com>
2023-12-23 22:00:15 -0300

    Initial Commit

---
+++ ToyRusher/ToyRusherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyRusherApp.swift
+//  ToyRusher
+//
+//  Created by Ieda Xavier on 23/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToyRusherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
